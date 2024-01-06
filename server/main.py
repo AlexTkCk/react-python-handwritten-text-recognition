34e5b37009397d9efce591817d6bf07b97547b40
Tkachenko Olexij <alextkachenco1233211@gmail.com>
2024-01-06 22:07:17 +0200

    Added block separating comments

--- server/main.py
+++ server/main.py
@@ -4,11 +4,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
+# ===== Data loading section =====
+
 mnist = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 x_train = tf.keras.utils.normalize(x_train, axis=1)
 x_test = tf.keras.utils.normalize(x_test, axis=1)
+
+# ===== End of Data loading section =====
+
+# ===== Train section =====
+
 #
 # model = tf.keras.models.Sequential()
 # model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
@@ -23,12 +30,17 @@ x_test = tf.keras.utils.normalize(x_test, axis=1)
 #
 # model.save('handwritten.model')
 
+# ===== End of Train section =====
+
+
+# ===== Evaluate section =====
+
 model = tf.keras.models.load_model('handwritten.model')
 
 image_number = 0
 while os.path.isfile(f"test_assets/image_{image_number}.png"):
     try:
-        img = cv2.imread(f"test_assets/image_{image_number}.png")[:,:,0]
+        img = cv2.imread(f"test_assets/image_{image_number}.png")[:, :, 0]
         img = np.invert(np.array([img]))
         prediction = model.predict(img)
         print('Prediction:', np.argmax(prediction))
@@ -38,3 +50,5 @@ while os.path.isfile(f"test_assets/image_{image_number}.png"):
         print('Error')
     finally:
         image_number += 1
+
+# ===== End of Evaluate section =====
