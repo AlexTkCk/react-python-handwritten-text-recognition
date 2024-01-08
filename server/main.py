9d742dceab6d37d89e405c8fe197716da7e41380
Tkachenko Olexij <alextkachenco1233211@gmail.com>
2024-01-08 15:08:29 +0200

    Added reset button

--- server/main.py
+++ server/main.py
@@ -4,7 +4,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 # import tensorflow as tf
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QVBoxLayout, QWidget
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QVBoxLayout, QWidget, QHBoxLayout
 from PyQt6 import QtGui
 from PyQt6.QtCore import Qt
 
@@ -19,19 +19,26 @@ class MainWindow(QMainWindow):
         self.prediction_result_label.setText('Prediction is :')
         self.canvas_label = QLabel()
         self.get_prediction_button = QPushButton()
+        self.reset_button = QPushButton()
         self.get_prediction_button.setText('Predict')
+        self.reset_button.setText('Reset')
         self.canvas = QtGui.QPixmap(300, 300)
         self.canvas.fill(Qt.GlobalColor.white)
 
         self.canvas_label.setPixmap(self.canvas)
 
-        layout = QVBoxLayout()
-        layout.addWidget(self.prediction_result_label)
-        layout.addWidget(self.canvas_label)
-        layout.addWidget(self.get_prediction_button)
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(self.prediction_result_label)
+        main_layout.addWidget(self.canvas_label)
+
+        buttons_layout = QHBoxLayout()
+        buttons_layout.addWidget(self.get_prediction_button)
+        buttons_layout.addWidget(self.reset_button)
+
+        main_layout.addLayout(buttons_layout)
 
         container = QWidget()
-        container.setLayout(layout)
+        container.setLayout(main_layout)
 
         self.setCentralWidget(container)
 
