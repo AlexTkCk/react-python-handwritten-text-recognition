69f03eec56ca2596544b13229607d18cf47487ff
Tkachenko Olexij <alextkachenco1233211@gmail.com>
2024-01-08 14:49:14 +0200

    Created a python demo GUI

--- server/main.py
+++ server/main.py
@@ -1,54 +1,43 @@
 import os
+import sys
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-import tensorflow as tf
-
-# ===== Data loading section =====
-
-mnist = tf.keras.datasets.mnist
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-x_train = tf.keras.utils.normalize(x_train, axis=1)
-x_test = tf.keras.utils.normalize(x_test, axis=1)
-
-# ===== End of Data loading section =====
-
-# ===== Train section =====
-
-#
-# model = tf.keras.models.Sequential()
-# model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
-# model.add(tf.keras.layers.Dense(128, activation='relu'))
-# model.add(tf.keras.layers.Dense(128, activation='relu'))
-# model.add(tf.keras.layers.Dense(128, activation='relu'))
-# model.add(tf.keras.layers.Dense(10, activation='softmax'))
-#
-# model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-#
-# model.fit(x_train, y_train, epochs=15)
-#
-# model.save('handwritten.model')
-
-# ===== End of Train section =====
+# import tensorflow as tf
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QVBoxLayout, QWidget
+from PyQt6 import QtGui
+from PyQt6.QtCore import Qt
 
 
-# ===== Evaluate section =====
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
 
-model = tf.keras.models.load_model('handwritten.model')
+        self.setWindowTitle('Digits recognition')
 
-image_number = 0
-while os.path.isfile(f"test_assets/image_{image_number}.png"):
-    try:
-        img = cv2.imread(f"test_assets/image_{image_number}.png")[:, :, 0]
-        img = np.invert(np.array([img]))
-        prediction = model.predict(img)
-        print('Prediction:', np.argmax(prediction))
-        plt.imshow(img[0], cmap=plt.cm.binary)
-        plt.show()
-    except:
-        print('Error')
-    finally:
-        image_number += 1
+        self.prediction_result_label = QLabel()
+        self.prediction_result_label.setText('Prediction is :')
+        self.canvas_label = QLabel()
+        self.get_prediction_button = QPushButton()
+        self.get_prediction_button.setText('Predict')
+        self.canvas = QtGui.QPixmap(300, 300)
+        self.canvas.fill(Qt.GlobalColor.white)
 
-# ===== End of Evaluate section =====
+        self.canvas_label.setPixmap(self.canvas)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.prediction_result_label)
+        layout.addWidget(self.canvas_label)
+        layout.addWidget(self.get_prediction_button)
+
+        container = QWidget()
+        container.setLayout(layout)
+
+        self.setCentralWidget(container)
+
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+
+app.exec()
