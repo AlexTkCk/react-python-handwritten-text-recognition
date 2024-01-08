f3e1341055e7717e3edb6219d317c878ba902c94
Tkachenko Olexij <alextkachenco1233211@gmail.com>
2024-01-08 15:18:12 +0200

    Added separating comments

--- server/main.py
+++ server/main.py
@@ -13,32 +13,47 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        # ==== Window Settings ====
         self.setWindowTitle('Digits recognition')
+        # ==== End of Window Settings ====
 
+        # ==== Prediction Label ====
         self.prediction_result_label = QLabel()
         self.prediction_result_label.setText('Prediction is :')
+        # ==== End of Prediction Label ====
+
+        # ==== Canvas ====
         self.canvas_label = QLabel()
+        self.canvas = QtGui.QPixmap(300, 300)
+        self.canvas.fill(Qt.GlobalColor.white)
+        self.canvas_label.setPixmap(self.canvas)
+        # ==== End of Canvas ====
+
+        # ==== Buttons ====
         self.get_prediction_button = QPushButton()
         self.reset_button = QPushButton()
         self.get_prediction_button.setText('Predict')
         self.reset_button.setText('Reset')
-        self.canvas = QtGui.QPixmap(300, 300)
-        self.canvas.fill(Qt.GlobalColor.white)
+        # ==== End of Buttons ====
 
-        self.canvas_label.setPixmap(self.canvas)
-
+        # ==== Main Layout ====
         main_layout = QVBoxLayout()
         main_layout.addWidget(self.prediction_result_label)
         main_layout.addWidget(self.canvas_label)
 
+        # ==== Button layout ====
         buttons_layout = QHBoxLayout()
         buttons_layout.addWidget(self.get_prediction_button)
         buttons_layout.addWidget(self.reset_button)
+        # ==== End of Button layout ====
 
         main_layout.addLayout(buttons_layout)
+        # ==== End of Main layout ====
 
+        # ==== Container ====
         container = QWidget()
         container.setLayout(main_layout)
+        # ==== End of container ====
 
         self.setCentralWidget(container)
 
