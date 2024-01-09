f79906bdaed03ab6e1bff51f0ed808bfaa64d13c
Tkachenko Olexij <alextkachenco1233211@gmail.com>
2024-01-10 00:14:13 +0200

    Created basic web-page template

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {useRef} from 'react';
 
+
+// Classname's are approximate - clarifying
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    const ref = useRef(null);
+    setTimeout(() => console.log(ref.current), 100)
+    return (
+        <div className="App">
+            <header className="header">
+                <h1 className="title">Handwritten digits recognition</h1>
+            </header>
+            <main className="main">
+                <div className={"canvas"}></div>
+                <div className="container">
+                    <h1 className={'container title'}>Prediction is : </h1>
+                    <div className="NN-visualization"></div>
+                    <div className="buttons-container">
+                        <button className={"predict-button"}>Predict</button>
+                        <button className={"reset-button"}>Clear canvas</button>
+                    </div>
+                </div>
+            </main>
+        </div>
+    );
 }
 
 export default App;
