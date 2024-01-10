377422b4c283df3bce53a4a89c8c17535312fd3d
Tkachenko Olexij <alextkachenco1233211@gmail.com>
2024-01-10 16:55:52 +0200

    Styled mobile design

--- client/src/App.js
+++ client/src/App.js
@@ -10,18 +10,18 @@ function App() {
             <header className="header h-fit py-2.5 px-3 mb-6">
                 <h1 className="title text-5xl md:text-8xl text-white text-center">Handwritten digits recognition</h1>
             </header>
-            <main className="main grow">
-                <Canvas className={"w-1/2 h-1/2"}/>
-                <div className="w-full">
-                    <div className="relative w-fit">
-                        <h1 className={'container_title relative text-5xl text-white bg-black md:text-8xl border border-sm border-white' +
+            <main className="main grow flex flex-col md:flex-row justify-center items-center gap-5">
+                <Canvas className={"w-full grow md:w-1/2"}/>
+                <div className="w-full flex flex-col md:flex-row items-center">
+                    <div className="relative w-fit mb-10">
+                        <h1 className={'container_title relative text-4xl sm:text-5xl text-white bg-black lg:text-6xl border border-sm border-white' +
                             ' rounded-bl-3xl text-center w-fit px-7 py-3 rounded-br-3xl z-[5]'}>Prediction is : ?</h1>
                         <div className="border-white absolute border left-5 top-5 w-full h-full rounded-br-3xl rounded-bl-3xl z-[3]"></div>
                     </div>
                     <div className="NN-visualization"></div>
-                    <div className="buttons-container flex flex-col md:flex-row gap-5 ">
-                        <button className={"predict-button text-6xl text-center bg-white text-black px-12 py-2.5 w-fit rounded-xl"}>Predict</button>
-                        <button className={"reset-button text-6xl text-center bg-white text-black px-12 py-2.5 w-fit rounded-xl"}>Clear canvas</button>
+                    <div className="buttons-container flex flex-col md:flex-row gap-5 items-center pb-3">
+                        <button className={"predict-button text-4xl md:text-6xl text-center bg-white text-black px-12 py-2.5 w-fit rounded-xl"}>Predict</button>
+                        <button className={"reset-button text-4xl md:text-6xl text-center bg-white text-black px-12 py-2.5 w-fit rounded-xl"}>Clear canvas</button>
                     </div>
                 </div>
             </main>
