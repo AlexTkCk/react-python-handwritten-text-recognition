1e66668c55c119e7d9f438168b9e9bfd15475283
Tkachenko Olexij <alextkachenco1233211@gmail.com>
2024-01-10 16:26:55 +0200

    Added canvas no logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import ParticleBackground from "./components/Particles/ParticleBackground";
+import Canvas from "./components/Canvas/Canvas";
 
 function App() {
     return (
-        <div className="App h-screen w-screen overflow-x-hidden">
+        <div className="App h-screen w-screen overflow-x-hidden flex flex-col">
             <ParticleBackground/>
 
             <header className="header h-fit py-2.5 px-3 mb-6">
-                <h1 className="title text-5xl lg:text-8xl text-white text-center">Handwritten digits recognition</h1>
+                <h1 className="title text-5xl md:text-8xl text-white text-center">Handwritten digits recognition</h1>
             </header>
             <main className="main grow">
-                <div className={"canvas"}></div>
+                <Canvas className={"w-1/2 h-1/2"}/>
                 <div className="w-full">
                     <div className="relative w-fit">
-                        <h1 className={'container_title relative text-5xl text-white bg-black lg:text-8xl border border-sm border-white' +
+                        <h1 className={'container_title relative text-5xl text-white bg-black md:text-8xl border border-sm border-white' +
                             ' rounded-bl-3xl text-center w-fit px-7 py-3 rounded-br-3xl z-[5]'}>Prediction is : ?</h1>
                         <div className="border-white absolute border left-5 top-5 w-full h-full rounded-br-3xl rounded-bl-3xl z-[3]"></div>
                     </div>
                     <div className="NN-visualization"></div>
-                    <div className="buttons-container">
-                        <button className={"predict-button"}>Predict</button>
-                        <button className={"reset-button"}>Clear canvas</button>
+                    <div className="buttons-container flex flex-col md:flex-row gap-5 ">
+                        <button className={"predict-button text-6xl text-center bg-white text-black px-12 py-2.5 w-fit rounded-xl"}>Predict</button>
+                        <button className={"reset-button text-6xl text-center bg-white text-black px-12 py-2.5 w-fit rounded-xl"}>Clear canvas</button>
                     </div>
                 </div>
             </main>
