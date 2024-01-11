6cd5e4843c093952e13cae45e8415afe49e95ffa
Tkachenko Olexij <alextkachenco1233211@gmail.com>
2024-01-11 12:06:46 +0200

    Created canvas context ref

--- client/src/components/Canvas/Canvas.js
+++ client/src/components/Canvas/Canvas.js
@@ -2,20 +2,27 @@ import React, {useEffect, useRef, useState} from 'react';
 
 const Canvas = (props) => {
     const canvasRef = useRef(null);
+    const contextRef = useRef(null);
+
+    const [isDrawing, setIsDrawing] = useState(false);
 
     useEffect(() => {
         const canvas = canvasRef.current;
         const context = canvas.getContext('2d');
         context.fillStyle = '#ffffff';
         context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+        context.lineCap = 'round';
+        context.lineWidth = 5;
+        context.strokeStyle = 'black';
+        contextRef.current = context;
     }, []);
 
     const startDrawing = () => {
-
+        setIsDrawing(true);
     }
 
     const finishDrawing = () => {
-
+        setIsDrawing(false);
     }
 
     const draw = () => {
