e697f4450673402031f7f607a41a256ce1b6b599
Tkachenko Olexij <alextkachenco1233211@gmail.com>
2024-01-11 14:39:32 +0200

    Added drawing for mobile

--- client/src/components/Canvas/Canvas.js
+++ client/src/components/Canvas/Canvas.js
@@ -3,7 +3,6 @@ import React, {useEffect, useRef, useState} from 'react';
 const Canvas = (props) => {
     const canvasRef = useRef(null);
     const contextRef = useRef(null);
-
     const [isDrawing, setIsDrawing] = useState(false);
 
     useEffect(() => {
@@ -38,12 +37,42 @@ const Canvas = (props) => {
         contextRef.current.stroke()
     }
 
+    const startDrawingTouch = (e) => {
+        const {clientX, clientY} = e.changedTouches[0];
+        const canvasRect = e.target.getBoundingClientRect();
+
+        const offsetX = clientX - canvasRect.left;
+        const offsetY = clientY - canvasRect.top;
+
+        setIsDrawing(true);
+        contextRef.current.beginPath();
+        contextRef.current.moveTo(offsetX, offsetY);
+    }
+
+
+    const drawTouch = (e) => {
+        if (!isDrawing) {
+            return
+        }
+        const {clientX, clientY} = e.changedTouches[0];
+        const canvasRect = e.target.getBoundingClientRect();
+
+        const offsetX = clientX - canvasRect.left;
+        const offsetY = clientY - canvasRect.top;
+        contextRef.current.lineTo(offsetX, offsetY);
+        contextRef.current.stroke()
+    }
+
     return (
         <canvas ref={canvasRef}
                 {...props}
                 onMouseDown={startDrawing}
                 onMouseUp={finishDrawing}
-                onMouseMove={draw}>
+                onMouseMove={draw}
+                onTouchStart={startDrawingTouch}
+                onTouchMove={drawTouch}
+                onTouchEnd={finishDrawing}
+        >
 
         </canvas>
     );
