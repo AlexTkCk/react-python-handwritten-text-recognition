656fc9615a316f7d68fb4298c846dd58c0c77cef
AlexTkCk <63850077+AlexTkCk@users.noreply.github.com>
2024-01-15 13:40:58 +0200

    Added prediction displaying

--- client/src/App.js
+++ client/src/App.js
@@ -29,7 +29,7 @@ function App() {
 
         fetch('http://localhost:8080/predict', requestOptions)
             .then(res => res.json())
-            .then(data => console.log(data));
+            .then(({prediction}) => setPredictedDigit(prediction));
 
     }
 
